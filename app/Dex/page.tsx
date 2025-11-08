72eef74a6d892aa4c5a3a61bd8afb64f0c5b7f62
V1JAYP <vijay.penumarthy07@gmail.com>
2025-11-08 17:25:06 -0600

    template fix

--- app/Dex/page.tsx
+++ app/Dex/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { House, PawPrint, Box, User } from "lucide-react";
+import { useEffect, useState } from "react";
 
 export default function BioDex() {
   return (
@@ -36,12 +37,41 @@ function Navbar() {
 
 
 function RightContainer() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    setIsVisible(true); // trigger slide-in after mount
+  }, []);
+
   return (
-    <div className=" w-[1000px] h-[600px] 
-      bg-surface-container-highest backdrop-blur-md 
-      border border-black rounded-3xl 
-      shadow-[0_0_40px_10px_rgba(255,255,255,0.05)]
-      " 
-    />
+    <div
+      className={`
+        w-[1000px] h-[600px] 
+        bg-surface-container-highest backdrop-blur-md 
+        border border-black rounded-3xl 
+        shadow-[0_0_40px_10px_rgba(255,255,255,0.05)]
+        transform transition-opacity: 30; transition-transform duration-1000 ease-out
+        ${isVisible ? "translate-x-0 opacity-100" : "-translate-x-full opacity-0"}
+      `}
+    >
+
+        <IndexContainer />
+    </div>
   );
+}
+
+function IndexContainer() {
+    return (
+        <div className="w-[140px] h-[220px] bg-tertiary-container border border-black rounded-3xl shadow-[0_0_40px_10px_rgba(255,255,255,0.05)]">
+            <Photo />
+        </div>
+    );
+}
+
+function Photo(){
+    return (
+        <div className="w-[120px] h-[140px] bg-white border border-black rounded-3xl shadow-[0_0_40px_10px_rgba(255,255,255,0.05) ] m-[8px]">
+        </div>
+    );
+
 }

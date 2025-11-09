91702f212b5fc667fea2898019ff0c68a5440b17
Quentin Bordelon <qgbordelon@gmail.com>
2025-11-08 20:37:07 -0600

    feat: Storage

--- src/Firebase/storage.ts
+++ src/Firebase/storage.ts
@@ -1,8 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getStorage, FirebaseStorage, ref, uploadBytes, getDownloadURL, deleteObject } from "firebase/storage";
-import * as fs from "fs";
-import * as path from "path";
-import * as os from "os";
+import { getStorage, ref, uploadBytes } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_KEY,
@@ -12,21 +9,19 @@ const firebaseConfig = {
   storageBucket: process.env.STORAGE_BUCKET,
   appId: process.env.APP_ID,
   measurementId: process.env.MEASUREMENT_ID,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  storageBucket: process.env.BUCKET_NAME
 };
 
+const app = initializeApp(firebaseConfig)
+const storage = getStorage(app)
 
-function createTempFile(content: string, extension: string = 'txt'): string {
-    const tempDir = "../Friebase";
-    const tempFilePath = path.join(tempDir, `tempFile_${uniqueId}.${extension}`);
+export async function addImageToStorage(img: Image) {
+  const storageRef = ref(storage, img);
 
-    fs.writeFileSync(tempFilePath, content);
-    return tempFilePath;
+  uploadBytes(storageRef, file)
+    .catch((error) => {
+      console.error(error);
+    })
 }
 
-const myTempFile = createTempFile('Hello, temporary file!', 'log');
-console.log(`Created temporary file: ${myTempFile}`);
-
-// To clean up later:
-// fs.unlinkSync(myTempFile);
-// console.log(`Deleted temporary file: ${myTempFile}`);

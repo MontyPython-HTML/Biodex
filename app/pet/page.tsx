15b96b1b3598f9ec6da724e52b6f73c9b94c7de9
willyt <williamabrad@gmail.com>
2025-11-08 20:20:53 -0600

    feat: expanded home page

---
+++ app/pet/page.tsx
@@ -0,0 +1,39 @@
+import Head from 'next/head'
+import { House, Box, PawPrint, User } from 'lucide-react';
+
+function Home() {
+  return (
+    <div className='bg-background w-full h-screen'>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+        <link href="https://fonts.googleapis.com/css2?family=Kreon:wght@300..700&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"></link>
+      </Head>
+      <nav className="flex flex-col bg-secondary-container w-[69px] justify-between items-center absolute h-screen px-[15px] py-[15px]">
+        <section id="topIcons" className='flex flex-col gap-5' color="white">
+          <House id="homeBtn" className='w-[39px] h-[39px]' color='white'/>  
+          <PawPrint id="petBtn" className='w-[39px] h-[39px] text-inverse-primary'/>
+          <Box id="inventoryBtn" className='w-[39px] h-[39px]' color="white"/>
+        </section>
+        <section id="profile">
+          <User id="profileBtn" className='w-[39px] h-[39px]' color="white"/>
+        </section>
+      </nav>
+      
+      <div className='flex flex-col'>
+        <div id="homePage" className='bg-[url(./media/leavesBkg.png)] bg-cover h-screen bg-right p-35 flex flex-col justify-center'>
+            <h1 id="mainTitle" className='font-(family-name:--font-kreon) text-[96px] float-left leading-25'>BioDex</h1>
+            <p id="aboutText" className='body-large w-[600px] gap-0'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nec molestie purus, sed porttitor urna. Nullam id odio at sem ultricies finibus. Donec lectus erat, pretium ac orci nec, sollicitudin vulputate velit. Etiam efficitur leo et sem cursus feugiat at ac mi. Nulla euismod viverra laoreet. Nulla tempus turpis quam, ut eleifend metus fermentum porttitor. Aenean tempor ligula lacinia risus tempus, eu scelerisque nibh molestie. Maecenas fermentum, tellus vitae consectetur pellentesque, enim urna accumsan massa, et rutrum quam mi non leo. Donec eu congue ipsum, eget iaculis ipsum. Nullam dignissim magna ultrices felis bibendum ultrices. Aliquam erat volutpat. In a magna sed erat mollis suscipit. Morbi eu bibendum justo.</p>
+        </div>
+
+        <div id="j" className='bg-[url(./media/leavesBkg.png)] bg-cover h-screen bg-right p-35 flex flex-col justify-center'>
+            <h1 id="y" className='font-(family-name:--font-kreon) text-[96px] float-left leading-25'>BioDex</h1>
+            <p id="s" className='body-large w-[600px] gap-0'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nec molestie purus, sed porttitor urna. Nullam id odio at sem ultricies finibus. Donec lectus erat, pretium ac orci nec, sollicitudin vulputate velit. Etiam efficitur leo et sem cursus feugiat at ac mi. Nulla euismod viverra laoreet. Nulla tempus turpis quam, ut eleifend metus fermentum porttitor. Aenean tempor ligula lacinia risus tempus, eu scelerisque nibh molestie. Maecenas fermentum, tellus vitae consectetur pellentesque, enim urna accumsan massa, et rutrum quam mi non leo. Donec eu congue ipsum, eget iaculis ipsum. Nullam dignissim magna ultrices felis bibendum ultrices. Aliquam erat volutpat. In a magna sed erat mollis suscipit. Morbi eu bibendum justo.</p>
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default Home

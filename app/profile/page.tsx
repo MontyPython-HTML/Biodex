f30b6240509656dfbe110796ebe40272a8fc02d6
V1JAYP <vijay.penumarthy07@gmail.com>
2025-11-09 04:10:01 -0600

    fixes profile

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -3,129 +3,81 @@ import { House, PawPrint, Box, User } from "lucide-react";
 import Link from "next/link";
 import gsap from "gsap";
 export default function Profile() {
-    return (
+return (
 <div>
-        <Navbar />
+<Navbar />
 
 <div className="flex justify-end w-full pr-10">
-    <div className="flex gap-6">
+<div className="flex gap-6">
 
-        <div className="w-220 h-40 bg-white shadow-xl rounded-2xl p-8 justify-let translate-y-5">
-        <h2 className="text-3xl font-bold text-center mb-6 text-gray-800 font-headline-large"> Welcome Back!, </h2>
-        <h3> HP: <HealthBar value={85} /></h3>
-        </div>
+<div className="w-220 h-40 bg-white shadow-xl rounded-2xl p-8 justify-let translate-y-5">
+<h2 className="text-3xl font-bold text-center mb-6 text-gray-800 font-headline-large"> Welcome Back!, </h2>
+<h3> HP: <HealthBar value={85} /></h3>
+</div>
 
-        <div className="w-full max-w-md bg-white shadow-xl rounded-2xl p-8">
-            <Circle />
-        <h3 className="text-3xl font-bold text-center mb-6 text-gray-800 font-headline-large"> Level Progress:<LevelBar level={8} currentXP={8} maxXP={20} /> </h3>
-            
-        </div>
+<div className="w-full max-w-md bg-white shadow-xl rounded-2xl p-8">
+<Circle />
+<h3 className="text-3xl font-bold text-center mb-6 text-gray-800 font-headline-large"> Level Progress:<LevelBar level={8} currentXP={8} maxXP={20} /> </h3>
+</div>
 
-    </div>
+</div>
 </div>
 
 <form />
 </div>
-    );
+);
 }
 
 function HealthBar({ value }) {
-  return (
-    <div className="w-64 h-6 bg-gray-800 rounded-full overflow-hidden border-2 border-black shadow-md">
-      <div
-        className="h-full bg-red-500 transition-all duration-500"
-        style={{ width: `${value}%` }}
-      ></div>
-    </div>
-  );
+return (
+<div className="w-64 h-6 bg-gray-800 rounded-full overflow-hidden border-2 border-black shadow-md">
+<div className="h-full bg-red-500 transition-all duration-500"style={{ width: `${value}%` }}></div>
+</div>
+);
 }
 
 
 function Navbar() {
-    return (
-    <nav className="flex flex-col bg-secondary-container w-[69px] justify-between items-center fixed h-screen px-[15px] py-[15px] z-900">
+return (
+<nav className="flex flex-col bg-secondary-container w-[69px] justify-between items-center fixed h-screen px-[15px] py-[15px] z-900">
 <section id="topIcons" className='flex flex-col gap-5'>
-          <Link href="/"><House id="homeBtn" className='w-[39px] h-[39px] text-inverse-primary'/> </Link> 
-          <Link href="/pet"><PawPrint id="petBtn" className='w-[39px] h-[39px]' color='white'/></Link> 
-          <Link href="/inventory"><Box id="inventoryBtn" className='w-[39px] h-[39px]' color="white"/></Link> 
-        </section>
-        <section id="profile">
-          <Link href="/profile"><User id="profileBtn" className='w-[39px] h-[39px]' color="white"/></Link>
-        </section>
-      </nav>
+<Link href="/"><House id="homeBtn" className='w-[39px] h-[39px] text-inverse-primary'/> </Link> 
+<Link href="/pet"><PawPrint id="petBtn" className='w-[39px] h-[39px]' color='white'/></Link> 
+<Link href="/inventory"><Box id="inventoryBtn" className='w-[39px] h-[39px]' color="white"/></Link> 
+</section>
+<section id="profile">
+<Link href="/profile"><User id="profileBtn" className='w-[39px] h-[39px]' color="white"/></Link>
+</section>
+</nav>
 
 
   );
 }
 function Circle(){
-    return (
-        <div className=" items-center mt-10 transalate-y-50">
+return (
+<div className=" items-center mt-10 transalate-y-50">
 <div className="w-100 h-100 bg-gray-500 rounded-full flex justify-center items-center">
 <p className="text-white text-lg">Circle Container</p>
 </div>
 </div>
-    );
-
-
-}
-
-function form(){
-    return (
-    <div>
-    <form className="space-y-5">
-
-<div>
-<label className="block text-gray-700 mb-1 font-medium">
-Email
-</label>
-<input
-type="email"
-              required
-              className="w-full px-4 py-2 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-600"
-              placeholder="yourname@email.com"
-            />
-          </div>
-          <div>
-            <label className="block text-gray-700 mb-1 font-medium">
-              Password
-            </label>
-            <input
-              type="password"
-              required
-              className="w-full px-4 py-2 rounded-xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-green-600"
-              placeholder="••••••••"
-            />
-          </div>
-          <button type="submit" className="w-full py-2 bg-secondary-container hover:bg-green-800 transition text-white rounded-xl font-semibold">Log In </button>
-          <p className="text-center text-sm text-gray-600">
-            Don't Have An Account?{" "}
-            <Link href="/sign" className="text-secondary-container font-medium hover:underline">
-              Sign Up!
-            </Link>
-          </p>
-        </form>
-        </div>
 );
 
+
 }
 
+
 function LevelBar({ level, currentXP, maxXP }) {
-  const percentage = (currentXP / maxXP) * 100;
+const percentage = (currentXP / maxXP) * 100;
 
-  return (
-    <div className="w-72 justify-center">
-      <p className="text-gray font-semibold mb-1">Level {level}</p>
+return (
+<div className="w-72 justify-center">
+<p className="text-gray font-semibold mb-1">Level {level}</p>
 
-      <div className="w-full h-5 bg-gray-700 rounded-full overflow-hidden border-2 border-black shadow-md">
-        <div
-          className="h-full bg-green-500 transition-all duration-500"
-          style={{ width: `${percentage}%` }}
-        ></div>
-      </div>
+<div className="w-full h-5 bg-gray-700 rounded-full overflow-hidden border-2 border-black shadow-md">
+<div className="h-full bg-green-500 transition-all duration-500" style={{ width: `${percentage}%` }}></div>
+</div>
 
-      <p className="text-white text-sm mt-1">
-        {currentXP} / {maxXP} XP
-      </p>
-    </div>
-  );
+<p className="text-white text-sm mt-1">{currentXP} / {maxXP} XP</p>
+</div>
+);
 }

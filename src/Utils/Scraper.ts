9112d64b0c96b569b5040703406fb42331586004
Blaze34536 <88854346+Blaze34536@users.noreply.github.com>
2025-11-08 22:56:48 -0600

    minor changes

--- src/Utils/Scraper.ts
+++ src/Utils/Scraper.ts
@@ -1,11 +1,15 @@
 import wtf from "wtf_wikipedia";
 
 export async function GetPlantInfo(plantName: string): Promise<void> {
-  const doc = await wtf.fetch(plantName, 'en')
-    .catch((error) => { console.error(error)});
+  const doc = await wtf.fetch(plantName)
+    .catch((error) => { console.error(error) });
+
   if (doc) {
-    console.log(doc.sentences()[0].text());
+    const singleDoc = Array.isArray(doc) ? doc[0] : doc;
+    if (singleDoc && singleDoc.sentences) {
+      console.log(singleDoc.sentences()[0].text());
+    }
   } else {
-    console.log("could not find Wikipedia page");
+    console.log("Could not find a valid Wikipedia document.");
   }
 }

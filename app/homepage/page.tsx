660683230109023bda9d9c05b28bd79bfba3f113
Nihal Mohapatro <88854346+Blaze34536@users.noreply.github.com>
2025-11-10 19:52:08 -0600

    Refactor homepage component and remove unused imports

--- app/homepage/page.tsx
+++ app/homepage/page.tsx
@@ -1,30 +1,15 @@
-'use client';
-import Image from 'next/image';
+"use client";
+
 import Head from 'next/head';
 import Link from 'next/link';
 import { House, Box, PawPrint, User } from 'lucide-react';
-import { identifyPlant } from "@/src/plant";    
-import * as auth from "@/src/Firebase/auth";
-import { useEffect, useRef, useState } from 'react';
+import { useRef } from 'react';
 import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-gsap.registerPlugin(ScrollTrigger);
-
-
 
 export default function Home () {
-  const aboutRef=useRef(null);
-  const signUpRef=useRef(null);
-  const getStartedRef=useRef(null);
-  useEffect(() => {
-    gsap.from("#mainTitle",{ x: -200, opacity: 0, duration: 1.5, ease: "power2.out" });
-    gsap.from("#aboutText",{ x: 200, opacity: 0,   duration: 1.5, ease: "power2.out", delay: 0.5, ease: "power3.out" });
-    gsap.from("#getStartedTitle",{ y: 100, opacity: 0, duration: 1.5, ease: "power2.out", scrollTrigger: { trigger: "#getStartedTitle", start: "top 80%", end: "bottom 60%", scrub: true } });
-    gsap.from("#signUpSurface",{ y: 100, opacity: 0, duration: 1.5, ease: "power2.out", delay: 0.5, scrollTrigger: { trigger: "#signUpSurface", start: "top 80%", end: "bottom 60%", scrub: true } });
-    gsap.from("#homePage",{ opacity: 0, duration: 2, ease: "power2.out" });
-
-
-  }, []);
+  const aboutRef = useRef(null);
+  const signUpRef = useRef(null);
+  const getStartedRef = useRef(null);
   return (
     <div className='bg-background w-full h-screen'>
       <Head>
@@ -39,7 +24,7 @@ export default function Home () {
           <Link href="/inventory"><Box id="inventoryBtn" className='w-[39px] h-[39px]' color="white"/></Link>
         </section>
         <section id="profile">
-          <Link href="/profile"><User id="profileBtn" className='w-[39px] h-[39px]' color="white"/></Link>
+          <User id="profileBtn" className='w-[39px] h-[39px]' color="white"/>
         </section>
       </nav>
       <div id='homePage'  className='flex flex-col '>
@@ -51,9 +36,7 @@ export default function Home () {
 
 <div id="signUpPage" ref={signUpRef} className="h-screen w-full bg-radial-[at_60%_30%] from-[#424A32] to-inverse-surface bg-cover flex flex-col items-center justify-center  px-[100px] overflow-hidden snap-always"> <div id="getStartedPage" ref={getStartedRef} className="flex flex-row items-center gap-20"> <h1 id="getStartedTitle" className="font-(family-name:--font-kreon) text-[10vmin] text-inverse-on-surface font-bold drop-shadow-[0_4px_10px_rgba(0,0,0,0.3)]">Get Started Today! </h1>
 <div id="signUpSurface" className="w-[35vw] h-[342px] bg-surface-container-high rounded-2xl shadow-xl flex flex-col items-center justify-center gap-6 backdrop-blur-md border border-white/10">
-<Link href="/sign">
-<button className="bg-tertiary-container text-on-tertiary-container  w-[28vw] h-20 rounded-xl font-(family-name:--font-poppins) text-[3vmin] hover:scale-105 transition-all duration-200 shadow-md">
-Sign Up</button></Link>
+<Link href="/sign"><button className="bg-tertiary-container text-on-tertiary-container  w-[28vw] h-20 rounded-xl font-(family-name:--font-poppins) text-[3vmin] hover:scale-105 transition-all duration-200 shadow-md">Sign Up</button></Link>
 <h2 className="display-medium text-on-surface-variant">or</h2>
 <Link href="/login"><button className="bg-tertiary-container text-on-tertiary-container w-[28vw] h-20 rounded-xl font-(family-name:--font-poppins) text-[3vmin] hover:scale-105 transition-all duration-200 shadow-md">Log In</button></Link>
 </div>
